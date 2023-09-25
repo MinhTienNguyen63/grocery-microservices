726c746f57a5dea170c9e04b0b6849d63d860187
Minh-Tien (Justin) Nguyen <112592656+MinhTienNguyen63@users.noreply.github.com>
2023-09-24 20:32:57 -0400

    Add files via upload

---
+++ cart_service.py
@@ -0,0 +1,96 @@
+# cart_service.py
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carts.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+PRODUCT_SERVICE_URL = 'http://127.0.0.1:5000/products/'
+
+
+# Cart Model
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String, unique=True, nullable=False)
+    items = db.Column(db.PickleType, nullable=False, default={})
+
+    def get_total_price(self):
+        total = 0
+        for product_id, quantity in self.items.items():
+            product = requests.get(PRODUCT_SERVICE_URL + str(product_id)).json()
+            if product:
+                total += product['price'] * quantity
+        return total
+
+
+@app.route('/cart/<user_id>', methods=['GET'])
+def get_cart(user_id):
+    cart = Cart.query.filter_by(user_id=user_id).first()
+    if not cart:
+        return jsonify({"error": "Cart not found"}), 404
+
+    cart_contents = {
+        "user_id": cart.user_id,
+        "items": cart.items,
+        "total_price": cart.get_total_price()
+    }
+    return jsonify(cart_contents)
+
+
+@app.route('/cart/<user_id>/add/<int:product_id>', methods=['POST'])
+def add_to_cart(user_id, product_id):
+    product = requests.get(PRODUCT_SERVICE_URL + str(product_id)).json()
+
+    if 'error' in product:
+        return jsonify({"error": product['error']}), 404
+
+    quantity_to_add = request.json.get('quantity', 1)
+    cart = Cart.query.filter_by(user_id=user_id).first()
+
+    if not cart:
+        cart = Cart(user_id=user_id)
+        db.session.add(cart)
+
+    if cart.items is None:
+        cart.items = {}
+
+    cart.items[product_id] = cart.items.get(product_id, 0) + quantity_to_add
+
+    db.session.commit()
+
+    return jsonify({
+        "message": "Added to cart",
+        "current_quantity": cart.items[product_id]
+    })
+
+
+@app.route('/cart/<user_id>/remove/<int:product_id>', methods=['POST'])
+def remove_from_cart(user_id, product_id):
+    quantity_to_remove = request.json.get('quantity', 1)
+    cart = Cart.query.filter_by(user_id=user_id).first()
+
+    if not cart:
+        return jsonify({"error": "Cart not found"}), 404
+
+    if product_id not in cart.items:
+        return jsonify({"error": "Product not in cart"}), 404
+
+    cart.items[product_id] -= quantity_to_remove
+    if cart.items[product_id] <= 0:
+        del cart.items[product_id]
+
+    db.session.commit()
+
+    return jsonify({
+        "message": "Removed from cart",
+        "remaining_quantity": cart.items.get(product_id, 0)
+    })
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(port=5001, debug=True)
---
+++ product_service.py
@@ -0,0 +1,50 @@
+# products.py
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.sqlite'
+db = SQLAlchemy(app)
+
+# Product Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'quantity': self.quantity
+        }
+
+# Endpoints
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    return jsonify([product.serialize() for product in products])
+
+@app.route('/products/<int:product_id>', methods=['GET'])
+def get_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify(product.serialize())
+    return jsonify({"error": "Product not found"}), 404
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    data = request.json
+    if not all(key in data for key in ('name', 'price', 'quantity')):
+        return jsonify({"error": "Name, Price, and Quantity are required"}), 400
+    product = Product(name=data['name'], price=data['price'], quantity=data['quantity'])
+    db.session.add(product)
+    db.session.commit()
+    return jsonify(product.serialize()), 201
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(port=5000, debug=True)
